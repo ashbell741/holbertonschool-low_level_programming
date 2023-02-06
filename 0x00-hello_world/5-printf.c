95ec5fed02ae09e7d3bc37466fa0fd111b5bff24
ashbell741 <ashbell741@gmail.com>
2023-02-06 10:17:16 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main-Entry
- * Return: Always 0
+ * Decription: main- prints out a string
+ * Return: 0 if successful
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a oiece of art,\n");
 	return (0);
+}
 
