2aa6e695d39e87bf219cd4834bb71a738cc66b91
ashbell741 <ashbell741@gmail.com>
2023-02-09 15:22:09 -0800

    print2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ * Description: prints the alaphabet
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
+
