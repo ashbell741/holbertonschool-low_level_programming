2f01ce6a691e7c3802f626e0866a8af857e75324
root <ashbell741gmail.com>
2023-05-29 11:44:10 -0700

    put

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-	char c[] = "_putchar";
+	char _putchar[9] ={ '_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	int i;
 
 	for (i = 0; i < 9; i++)
 	{
-		_putchar(c[i]);
+		_putchar(_putchar[i]);
 	}
 	_putchar('\n');
 	return (0);
