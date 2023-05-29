edd879ef3d300a1e5bf1fd3ddb1bf7b25773b160
root <ashbell741gmail.com>
2023-05-29 14:52:27 -0700

    lo

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  	* main - Entry point
@@ -6,7 +6,7 @@
 	* Return: Always 0
 	*/
 int main(void)
-
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
